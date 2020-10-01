15e13cee2510c59a621287c394b5f71b24066f68
Owen Wexler <owenbwexler@gmail.com>
2020-10-01 15:31:37 -0400

    Add dynamic data to frontend design - Refactor modal styles into separate file

---
+++ client/src/components/styledComponents/ModalStyles.jsx
@@ -0,0 +1,17 @@
+const MODAL_STYLE = {
+  position: 'fixed',
+  padding: '.2% 3%',
+  zIndex: 1000
+};
+
+const OVERLAY_STYLE = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, .89)',
+  zIndex: 1000
+};
+
+export { MODAL_STYLE, OVERLAY_STYLE }
