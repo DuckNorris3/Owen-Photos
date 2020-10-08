51f04b0bdd4f3f85bafac07599c233c3d876db02
Owen Wexler <owenbwexler@gmail.com>
2020-10-08 14:11:33 -0400

    Initalize dot-env throughout service

--- db/db.js
+++ db/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const mongooseUri = 'mongodb://localhost/tenthop-photos'
+dotenv.config();
+
+const mongooseUri = process.env.MONGOOSE_URI;
 
 const db = mongoose.connect(mongooseUri);
 
