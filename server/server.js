77bdc78d40c0608e66c0c8cba4b1226858899719
Owen Wexler <owenbwexler@gmail.com>
2020-10-10 14:20:09 -0400

    Add dot-env to server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
 const app = require('./app.js');
+const dotenv = require('dotenv');
+dotenv.config();
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log('PhotoCarousel server listening on ', PORT));
